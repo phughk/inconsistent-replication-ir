dce01f8c1d7748527f12ca74ec90e8727f184f10
Hugh Kaznowski <phkaznowski@gmail.com>
2024-09-26 21:22:21 +0100

    Update fuzzer, move linearizable computer

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,5 +1,8 @@
 #![no_main]
 
+mod linearizable_compute;
+
+use crate::linearizable_compute::{LinearizableComputeOperation, LinearizableComputer};
 use arbitrary::{Arbitrary, Unstructured};
 use inconsistent_replication_ir::test_utils::{MockIRNetwork, MockIRStorage};
 use inconsistent_replication_ir::types::DecideFunction;
@@ -32,16 +35,20 @@ impl<'a> Arbitrary<'a> for TestScenario {
 
 #[derive(Arbitrary, Debug)]
 enum TestStep {
-    InconsistentMessage { client: usize, message: Message },
-    ConsistentMessage { client: usize, message: Message },
-    DropRequest { who: usize },
-    DropResponse { who: usize },
-}
-
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Arbitrary)]
-pub enum Message {
-    Request(RequestPayload),
-    Response(ResponsePayload),
+    InconsistentMessage {
+        client: usize,
+        message: LinearizableComputeOperation,
+    },
+    ConsistentMessage {
+        client: usize,
+        message: LinearizableComputeOperation,
+    },
+    DropRequest {
+        who: usize,
+    },
+    DropResponse {
+        who: usize,
+    },
 }
 
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Arbitrary)]
@@ -57,33 +64,49 @@ pub struct ResponsePayload {
 }
 
 pub struct TestDecideFunction {
-    request: Message,
+    request: LinearizableComputeOperation,
 }
-impl DecideFunction<Message> for TestDecideFunction {
-    fn decide<'a, S: IntoIterator<Item = &'a Message>>(&self, _values: S) -> &'a Message {
-        let values: Vec<&'a Message> = _values.into_iter().collect();
-        values.first().unwrap()
+impl DecideFunction<LinearizableComputeOperation> for TestDecideFunction {
+    fn decide<'a, S: IntoIterator<Item = &'a LinearizableComputeOperation>>(
+        &self,
+        choices: S,
+    ) -> &'a LinearizableComputeOperation {
+        choices.into_iter().next().unwrap()
     }
 }
 
 fuzz_target!(|data: TestScenario| {
     // Create cluster
-    let network = MockIRNetwork::<_, _, MockIRStorage<_, _>>::new();
+    let network = MockIRNetwork::<_, _, MockIRStorage<_, _, LinearizableComputer>>::new();
+    let members: Vec<usize> = (0..data.nodes).collect();
     for i in 0..data.nodes {
         smol::block_on(async {
             network.register_node(
                 i,
-                InconsistentReplicationServer::new(network.clone(), MockIRStorage::new(), i).await,
+                InconsistentReplicationServer::new(
+                    network.clone(),
+                    MockIRStorage::new(members.clone(), LinearizableComputer::new()),
+                    i,
+                )
+                .await,
             );
         })
     }
     let mut clients = Vec::with_capacity(data.clients);
     for client_id in 0..data.clients {
-        clients.push(InconsistentReplicationClient::new(
-            network.clone(),
-            MockIRStorage::new(),
-            client_id,
-        ));
+        // TODO maybe no need to clone as same thread and blocking
+        let network_clone = network.clone();
+        let members_clone = members.clone();
+        let client = smol::block_on(async move {
+            InconsistentReplicationClient::new(
+                network_clone,
+                // The computer isn't used on the client, but it is part of the shared storage interface
+                MockIRStorage::new(members_clone, LinearizableComputer::new()),
+                client_id,
+            )
+            .await
+        });
+        clients.push(client);
     }
 
     // Run scenario
